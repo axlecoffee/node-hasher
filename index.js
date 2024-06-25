079680294e86ba7d11b0d6d393bd04d706944d91
Axle <95949634+axlecoffee@users.noreply.github.com>
2024-06-24 22:24:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+const inputDir = './input';
+const outputDir = './output';
+
+fs.readdir(inputDir, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const hashes = [];
+
+  files.forEach((file) => {
+    const filePath = path.join(inputDir, file);
+    const fileExt = path.extname(file);
+
+    if (fileExt === '.jar') {
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const hash = crypto.createHash('sha256');
+        hash.update(data);
+        const sha256 = hash.digest('hex');
+
+        hashes.push(`${file}: ${sha256}`);
+
+        // Write the hashes to the file as they are generated
+        fs.appendFile(path.join(outputDir, 'hashes.txt'), `${file}: ${sha256}\n`, (err) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      });
+    }
+  });
+});
